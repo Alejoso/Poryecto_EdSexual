f1b789197a3b14a899cd8010974ee81c5e99a910
Alejoso <159715523+Alejoso@users.noreply.github.com>
2024-04-24 18:04:09 -0500

    Delete Informacion.java

--- Informacion.java
+++
@@ -1,9 +0,0 @@
-public class Informacion {
-    public String informacionETS;
-    public String informacionAnticonceptivos; 
-    public String fuentesDeInformacion; 
-
-    public static void generarInformacion () {
-        //Podriamos decir que hay un array de mensages, y el programa elige un indice random para mostrar tal mensaje
-    }
-}
